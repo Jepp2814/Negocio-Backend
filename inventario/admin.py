8b9ab394e39bf04279e875101c2385b074e695ce
Giovanna24045 <gsalazarb@unemi.edu.ec>
2026-06-26 23:39:37 -0500

    Update admin.py

--- inventario/admin.py
+++ inventario/admin.py
@@ -1,8 +1,69 @@
 from django.contrib import admin
-from .models import Producto
+from .models import Producto, Proveedor, Categoria
 
+# 1. PRODUCTO
 @admin.register(Producto)
 class ProductoAdmin(admin.ModelAdmin):
-    list_display = ['codigo', 'nombre', 'stock', 'costo', 'precio_venta']
-    list_filter = ['stock']
-    search_fields = ['codigo', 'nombre']
+
+    list_display = (
+        "codigo",
+        "nombre",
+        "marca",
+        "modelo", 
+        "precio_venta",
+        "stock",
+        "estado",
+    )
+
+    search_fields = (
+        "codigo",
+        "nombre",
+        "marca",
+        "modelo", 
+    )
+
+    list_filter = (
+        "marca",
+    )
+
+    ordering = (
+        "nombre",
+    )
+
+# 2. PROVEEDOR
+@admin.register(Proveedor)
+class ProveedorAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "nombre",
+        "telefono",
+        "correo",
+        "activo",
+    )
+
+    search_fields = (
+        "nombre",
+        "telefono",
+        "correo",
+    )
+
+    ordering = (
+        "nombre",
+    )
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "nombre",
+        "activo",
+    )
+
+    search_fields = (
+        "nombre",
+    )
+
+    ordering = (
+        "nombre",
+    )
+    
