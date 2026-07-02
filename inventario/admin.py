0cd2f35bed4f84a9568d8898d72734b1331f0b85
Giovanna24045 <gsalazarb@unemi.edu.ec>
2026-07-01 23:36:01 -0500

    Update admin.py

--- inventario/admin.py
+++ inventario/admin.py
@@ -1,10 +1,99 @@
 from django.contrib import admin
-from .models import Producto
+from .models import Producto, Proveedor, Categoria, Cliente
 
 # 1. PRODUCTO
 @admin.register(Producto)
 class ProductoAdmin(admin.ModelAdmin):
-    list_display = ['codigo', 'nombre', 'stock', 'precio_venta', 'created_at']
-    search_fields = ['codigo', 'nombre']
-    ordering = ['-created_at']
-    readonly_fields = ['created_at', 'updated_at']
+
+    list_display = (
+        "codigo",
+        "nombre",
+        "marca",
+        "modelo", 
+        "precio_venta",
+        "stock",
+        "estado",
+    )
+
+    search_fields = (
+        "codigo",
+        "nombre",
+        "marca",
+        "modelo", 
+    )
+
+    list_filter = (
+        "marca",
+    )
+
+    ordering = (
+        "nombre",
+    )
+
+# 2. PROVEEDOR
+@admin.register(Proveedor)
+class ProveedorAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "nombre",
+        "telefono",
+        "correo",
+        "activo",
+    )
+
+    search_fields = (
+        "nombre",
+        "telefono",
+        "correo",
+    )
+
+    ordering = (
+        "nombre",
+    )
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "nombre",
+        "activo",
+    )
+
+    search_fields = (
+        "nombre",
+    )
+
+    ordering = (
+        "nombre",
+    )
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "cedula",
+        "primer_apellido",
+        "segundo_apellido",
+        "primer_nombre",
+        "segundo_nombre",
+        "telefono",
+        "activo",
+    )
+
+    search_fields = (
+        "cedula",
+        "primer_nombre",
+        "segundo_nombre",
+        "primer_apellido",
+        "segundo_apellido",
+        "telefono",
+        "correo",
+    )
+
+    list_filter = (
+        "activo",
+    )
+
+    ordering = (
+        "primer_apellido",
+        "primer_nombre",
+    )
